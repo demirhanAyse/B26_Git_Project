cb77b85d2ced8b5f37dd1e1987764766aa79ef4c
demirhanAyse <98864209+demirhanAyse@users.noreply.github.com>
2022-03-26 16:31:15 -0400

    Create JamesBond.java

---
+++ src/day0/JamesBond.java
@@ -0,0 +1,13 @@
+package day0;
+
+
+public class James Bond
+  
+  public static void main(String [] args){
+  System.out.println("James Bond)
+    }
+   }
+                     
+                     
+    
+  
